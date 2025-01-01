241c00d5f6f6e48ff3e74ec2a4b951d0b8553687
Aisha630 <ayeshashafique123@gmail.com>
2025-01-01 13:18:59 +0500

    test: :construction_worker: Upgrade to v4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # List of Streamlit App URLs
 
 STREAMLIT_APPS = [
-"https://claudeca-g6hfckisd2bi6djneg2edb.streamlit.app/",
-"https://m8ax2ke8rmfr3lmvqqxl4x.streamlit.app/"
+"https://claudeca-g6hfckisd2bi6djneg2edb.streamlit.app/"
 ]
 
